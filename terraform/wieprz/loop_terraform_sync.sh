7620f2c97d97094bfd1dde873ad4512a0de47b6d
kmonticolo <kmonticolo@gmail.com>
2025-10-08 20:53:21 +0000

    add conditional to not run when backup is performed

--- terraform/wieprz/loop_terraform_sync.sh
+++ terraform/wieprz/loop_terraform_sync.sh
@@ -2,8 +2,14 @@
 
 cd /root/homelab-public/terraform/wieprz || exit 1
 
+current_hour=$(date +%H)
+
+if [ "$current_hour" -ge 19 ] && [ "$current_hour" -lt 22 ]; then
+  exit 0
+else
 while true; do
     echo "=== \$(date '+%Y-%m-%d %H:%M:%S') ===" >> terraform_sync.log
     pytest -s test_terraform_sync.py >> terraform_sync.log 2>&1
-    sleep 3
+    sleep 60
 done
+fi
